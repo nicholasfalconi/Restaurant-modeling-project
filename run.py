a66adc8d10e01b5fa4a8bca52fba455442eb1919
Witts-A <71847614+Witts-A@users.noreply.github.com>
2020-11-01 16:57:50 -0500

    run.py push
    switching to desktop pc, needed this file, NOT for monday submissiom

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
-
 from nnf import Var
 from lib204 import Encoding
 
-# Call your variables whatever you want
-a = Var('a')
-b = Var('b')
-c = Var('c')
-x = Var('x')
-y = Var('y')
-z = Var('z')
+# Variables for our 
+# price
+price = []
+for i in range(2):
+    price.append(Var(f"price_{i}"))
 
+# index 0 - 3 with each index corresponding to a certain dietary restriction
+dietrestrictions = []
+for i in range(3):
+    dietrestrictions.append(Var(f"dietrstrct_{i}"))
 
 #
 # Build an example full theory for your setting and return it.
@@ -19,9 +20,28 @@ z = Var('z')
 #  what the expectations are.
 def example_theory():
     E = Encoding()
-    E.add_constraint(a | b)
-    E.add_constraint(~a | ~x)
-    E.add_constraint(c | y | z)
+    # reference for my peanut brain
+    # E.add_constraint(a | b)
+    # E.add_constraint(~a | ~x)
+    # E.add_constraint(c | y | z)
+
+    # this is where our theory starts
+
+    # price constraints
+    E.add_constraint(price[0] | price[1] | price[2])
+    E.add_constraint((price[0] & price[1]).negate())
+    E.add_constraint((price[0] & price[2]).negate())
+    E.add_constraint((price[1] & price[2]).negate())
+    E.add_constraint((price[0] & price[1] & price[2]).negate())
+
+    # diet constraints
+    E.add_constraint(dietrestrictions[0] | dietrestrictions[1] | dietrestrictions[2] | dietrestrictions[3])
+    E.add_constraint((dietrestrictions[1] & dietrestrictions[2]).negate())
+    E.add_constraint((dietrestrictions[2] & dietrestrictions[3]).negate())
+    E.add_constraint((dietrestrictions[0] & dietrestrictions[1]).negate())
+
+    
+
     return E
 
 
