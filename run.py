a51c1f48824d617e68ae76515fa7c18e5be76687
nicholasfalconi <69697951+nicholasfalconi@users.noreply.github.com>
2020-12-09 23:39:56 -0500

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from nnf import Var
 from lib204 import Encoding
+from csvReader import readCSV
 
 '''
 Customer class
@@ -12,125 +13,124 @@ Paramaters:
     dine_opt: preferred dining options
 '''
 class customer:
-    def __init__(self, price_opt, diet_opt, dine_opt):
+    def __init__(self, price_opt, diet_opt, dine_opt,distance):
         self.userprice = price_opt
         self.userdiet = diet_opt
         self.userdine_opt = dine_opt
+        self.distance = distance
 
+#Users option selection
 
+#Restaurant
 
+def priceConstraint(restaurant,customer):
+      if customer.userPrice == "low":
+        if restaurant.price == "low":
+            return low & ~med & ~high
+        else:
+            return low & ~med & ~high & ~low
 
-# # Have this here for reference, I know it is not how its supposed to work using this lib
+    if customer.userPrice == "med":
+        if restaurant.price == "med":
+            return med & ~high
+        else:
+            return med & ~high & ~high
 
-# # Propositions
-# # price
-# price = []
-# for i in range(3):
-#     price.append(Var(f"price_{i}"))
+    if customer.userPrice == "high":
+        if restaurant.price == "high":
+            return high
+        else:
+            return ~high
 
-# # diet restrictions
-# # index 0 - 3 with each index corresponding to a certain dietary restriction
-# # index 0 = gluten free, index 1 = vegan, index 2 = vegetrarian, index 3 = lactose
-# diettype = []
-# for i in range(4):
-#     diettype.append(Var(f"diettype_{i}"))
+def glutenConstraint(restaurant,customer):
+    if 'gluten' in customer.userdiet:
+        if restaurant.diet[2] == 'TRUE':
+            return gluten
+    else:
+        return False
 
-# dietrestrictions = []
-# for i in range(4):
-#     dietrestrictions.append(Var(f"dietrstrct_{i}"))
+def lactoseConstraint(restaurant,customer):
+    if 'lactose' in customer.userdiet:
+        if restaurant.diet[3] == 'TRUE':
+            return lactose
+    else:
+        return ~lactose
+def veganConstraint(restaurant,customer):
+    if 'vegan' in customer.userdiet:
+        if restaurant.diet[0] == 'TRUE':
+            return vegan
+    else:
+        return ~vegan
+def vegetarianConstraint(restaurant,customer):
+    if 'vegeterian' in customer.userdiet:
+        if restaurant.diet[1] == 'TRUE':
+            return vegetarian
+    else:
+        return ~vegetarian
 
-# # Dine-in, take-out, delivery
-# # index 0 = dine-in, index 1 = take-out, index 2 = delivery
-# dine_options = []
-# for i in range(3):
-#     dine_options.append(Var(f"dine_opt_{i}"))
+def dineInConstraint(restaurant,customer):
+    if customer.userdine_opt == 'dine-in':
+        if restaurant.delivery[0] == 'TRUE':
+            return dine_in & ~take_out & ~delivery
+        else:
+            return dine_in & ~dine_in
 
+def takeOutConstraint(restaurant,customer):
+    if customer.userdine_opt == 'take-out':
+        if restaurant.delivery[1] == 'TRUE':
+            return take_out & ~dine_in & ~delivery
+        else:
+            return take_out & ~take_out
 
-def example_theory():
+def deliveryConstraint(restaurant,customer):
+    if customer.userdine_opt == 'delivery':
+        if restaurant.delivery[2] == 'TRUE':
+            return delivery & ~take_out & ~dine_in
+        else:
+            return delivery & ~delivery
 
-    # Have this here for reference, I know it is not how its supposed to work using this lib
+def distanceConstraint(restaurant,customer):
+    if customer.distance == 'under 10':
+        if restaurant.distance[0] == 'TRUE':
+            return time_under_10 & ~time_10_to_20 & ~time_over_20
+        else:
+            return time_under_10 & ~time_under_10
 
-    # Propositions
-    # price
-    price = []
-    for i in range(3):
-        price.append(Var(f"price_{i}"))
+    if customer.distance == '10 to 20':
+        if restaurant.distance[1] == 'TRUE':
+            return time_10_to_20 & ~time_under_10 & ~time_over_20
+        else:
+            return time_10_to_20 & ~time_10_to_20
 
-    # diet restrictions
-    # index 0 - 3 with each index corresponding to a certain dietary restriction
-    # index 0 = gluten free, index 1 = vegan, index 2 = vegetrarian, index 3 = lactose
-    diettype = []
-    for i in range(4):
-        diettype.append(Var(f"diettype_{i}"))
+    if customer.distance == 'over 20':
+        if restaurant.distance[2] == 'TRUE':
+            return time_over_20 & ~time_10_to_20 & ~time_under_10
+        else:
+            return time_over_20 & ~time_over_20
 
-    dietrestrictions = []
-    for i in range(4):
-        dietrestrictions.append(Var(f"dietrstrct_{i}"))
-
-    # Dine-in, take-out, delivery
-    # index 0 = dine-in, index 1 = take-out, index 2 = delivery
-    dine_options = []
-    for i in range(3):
-        dine_options.append(Var(f"dine_opt_{i}"))
-
-
+def example_theory(restaurant,customer):
+    
+    r = restaurant
+    c = customer
     E = Encoding()
 
-    # this is where our theory starts
-
-    # price constraints
-    # pretty self explanitory
-    E.add_constraint(price[0] | price[1] | price[2])
-    E.add_constraint((price[0] & price[1]).negate())
-    E.add_constraint((price[0] & price[2]).negate())
-    E.add_constraint((price[1] & price[2]).negate())
-    E.add_constraint((price[0] & price[1] & price[2]).negate())
-
-    # diet constraints
-
-    # accommodate one restrictions
-    E.add_constraint(dietrestrictions[0] | dietrestrictions[1] | dietrestrictions[2] | dietrestrictions[3])
-    # accommodate all of the restrictions
-    E.add_constraint(dietrestrictions[0] & dietrestrictions[1] & dietrestrictions[2] & dietrestrictions[3])
-
-    # accommodate two of the restrictions
-    E.add_constraint(dietrestrictions[0] & (diettype[1] | diettype[2] | diettype[3]))
-    E.add_constraint(dietrestrictions[1] & (diettype[0] | diettype[2] | diettype[3]))
-    E.add_constraint(dietrestrictions[2] & (diettype[0] | diettype[1] | diettype[3]))
-    E.add_constraint(dietrestrictions[3] & (diettype[0] | diettype[1] | diettype[2]))
-
-    # accommodate three of the restrictions (Yes I know they don't go in logical order this is just what I have written down)
-    # accommodate three of the restrictions (Yes I know they don't go in logical order this is just what I have written down)
-    E.add_constraint(dietrestrictions[1] & dietrestrictions[3] & (diettype[0] | diettype[2]))
-    E.add_constraint(dietrestrictions[1] & dietrestrictions[0] & (diettype[3] | diettype[2]))
-    E.add_constraint(dietrestrictions[1] & dietrestrictions[2] & (diettype[3] | diettype[0]))
-
-    # Eating location? not too sure what to call this
-    # all three (dine-in, take-out and delivery)
-    E.add_constraint(dine_options[0] & dine_options[1] & dine_options[2])
-
-    # must be one
-    E.add_constraint(dine_options[0] | dine_options[1] | dine_options[2])
-
-    # can be two
-    E.add_constraint(dine_options[0] & (dine_options[1] | dine_options[2]))
-    E.add_constraint(dine_options[1] & (dine_options[0] | dine_options[2]))
-    E.add_constraint(dine_options[2] & (dine_options[0] | dine_options[1]))
-
+    E.add_constraint(priceConstraint(r,c) | dineInConstraint(r,c) | takeOutConstraint(r,c) | deliveryConstraint(r,c) | distanceConstraint(r,c) | glutenConstraint(r,c) | lactoseConstraint(r,c) | veganConstraint(r,c) | vegetarianConstraint(r,c))
 
     return E
 
+        
+
 
 if __name__ == "__main__":
-    
     # This is where we will get user input information and whatnot
     flag = True
+    restaurant_list = readCSV()
 
     # While loop to start
     while flag:
 
         # creating example theory
-        T = example_theory()
+        # T = example_theory()
         # Asking if user wants to continue or exit
         prog_exit = input('Welcome to the Queens restuarant finder! Press Q to quit or enter to continue.\n')
 
@@ -145,10 +145,13 @@ if __name__ == "__main__":
         # Telling user which price was selected as well as some exception handling
         if user_price in [1,2,3]:
             if user_price == 1:
+                price = 'low'
                 print('You selected $.')
             elif user_price == 2:
+                price = 'med'
                 print('You selected $$.')
             else:
+                price = 'high'
                 print('You selected $$$')
         else:
             print('Invalid input: Must be either option 1, 2 or 3')
@@ -166,33 +169,56 @@ if __name__ == "__main__":
         for entry in range(len(user_selected_restrictions)):
             user_selected_restrictions[entry] = int(user_selected_restrictions[entry])
 
+        diet = []
+        if 1 in user_selected_restrictions:
+            diet.append('vegan')
+        elif 2 in user_selected_restrictions:
+            diet.append('vegetarian')
+        elif 3 in user_selected_restrictions:
+            diet.append('gluten')
+        elif 4 in user_selected_restrictions:
+            diet.append('lactose')
+
         # Getting user preference for dining options
         user_dine_option = int(input('Please select a dining option: \n 1. Dine-in \n 2. Take-out\n 3. Delivery\n'))
 
+        if user_dine_option == 1:
+            dining = 'dine-in'
+        elif user_dine_option == 2:
+            dining = 'take-out'
+        else:
+            dining = 'delivery'
         # Getting user preference for distance
         user_distance_option = int(input('Please select a distance from Queens campus:'
         ' \n 1. Under 10 minutes \n 2. Between 10 and 20 minutes \n 3. Over 20 minutes\n'))
 
+        if user_distance_option == 1:
+            distance = 'under 10'
+        elif user_distance_option == 2:
+            distance = '10 to 20'
+        else:
+            distance = 'over 20'
+
+
         # Creating customer class to store information in an object for easier access
-        user = customer(user_price, user_selected_restrictions, user_dine_option)
+        user = customer(price, diet, user_dine_option, distance)
         
-        print("\nSatisfiable: %s" % T.is_satisfiable())
-        print("# Solutions: %d" % T.count_solutions())
-        print("   Solution: %s" % T.solve())
+        # Need to iterate through the list and find which restaurants match with the users preference
+        # using the example theory function. Use T.solve to find the solution to the users preferences and then match with
+        # restaurants that match up
 
+        # T = example_theory(user)
+        # dictionary with the mappings of the solution
+        # examplesolution = T.solve()
 
+        scores = {}
+        finalListR = []
+        for entry in restaurant_list:
+            # iterating through each restaurant
+            retaurant = example_theory(entry,user)
+            entry.is_satisfiable()
+            scores[entry.name] = entry.count_solutions()
 
-    # T = example_theory()
-
-    # print("\nSatisfiable: %s" % T.is_satisfiable())
-    # print("# Solutions: %d" % T.count_solutions())
-    # print("   Solution: %s" % T.solve())
-
-'''
-Haven't implemented this w/ our variables
-    print("\nVariable likelihoods:")
-    
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        print(" %s: %.2f" % (vn, T.likelihood(v)))
-    print()
-'''
+        
+        for key, value in scores.items():
+            print(key, ' : ', value)
